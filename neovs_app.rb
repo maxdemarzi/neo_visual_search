338bade5cdba64725dc077ded51bc5def9e1c28c
Max De Marzi <maxdemarzi@hotmail.com>
2013-06-20 11:24:50 -0500

    values working

--- neovs_app.rb
+++ neovs_app.rb
@@ -23,5 +23,34 @@ class App < Sinatra::Base
     cache_control :public, :must_revalidate, :max_age => 600
     $neo.list_labels.to_json    
   end
+
+  get '/values/:facet/' do
+    content_type :json
+    
+    search_key = $neo.get_schema_index(params[:facet]).first["property-keys"].first
+    
+    cypher = "MATCH node:#{params[:facet]} 
+              RETURN node.#{search_key} AS label
+              ORDER BY label
+              LIMIT 25"
+    
+    values = $neo.execute_query(cypher)["data"].flatten.to_json
+  end
+
+
+  get '/values/:facet/:term' do
+    content_type :json
+    
+    search_key = $neo.get_schema_index(params[:facet]).first["property-keys"].first
+    
+    cypher = "MATCH node:#{params[:facet]} 
+              WHERE node.#{search_key} =~ {term}
+              RETURN node.#{search_key} AS label
+              ORDER BY label
+              LIMIT 25"
+    
+    values = $neo.execute_query(cypher, {:term => "(?i)" + params[:term] + ".*"})["data"].flatten.to_json
+  end
+
   
 end
--- public/js/neo_visualsearch.js
+++ public/js/neo_visualsearch.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
     var visualSearch = VS.init({
       container  : $('#search_box_container'),
-      query      : 'country: "United States" "U.S. State": "New York" account: 5-samuel',
+      query      : '',
       showFacets : true,
       unquotable : [
         'text',
@@ -40,92 +40,13 @@ $(document).ready(function() {
           callback(facets);
         },
         valueMatches : function(facet, searchTerm, callback) {
-          switch (facet) {
-          case 'account':
-              callback([
-                { value: '1-amanda', label: 'Amanda' },
-                { value: '2-aron',   label: 'Aron' },
-                { value: '3-eric',   label: 'Eric' },
-                { value: '4-jeremy', label: 'Jeremy' },
-                { value: '5-samuel', label: 'Samuel' },
-                { value: '6-scott',  label: 'Scott' }
-              ]);
-              break;
-            case 'filter':
-              callback(['published', 'unpublished', 'draft']);
-              break;
-            case 'access':
-              callback(['public', 'private', 'protected']);
-              break;
-            case 'title':
-              callback([
-                'Pentagon Papers',
-                'CoffeeScript Manual',
-                'Laboratory for Object Oriented Thinking',
-                'A Repository Grows in Brooklyn'
-              ]);
-              break;
-            case 'city':
-              callback([
-                'Cleveland',
-                'New York City',
-                'Brooklyn',
-                'Manhattan',
-                'Queens',
-                'The Bronx',
-                'Staten Island',
-                'San Francisco',
-                'Los Angeles',
-                'Seattle',
-                'London',
-                'Portland',
-                'Chicago',
-                'Boston'
-              ]);
-              break;
-            case 'U.S. State':
-              callback([
-                "Alabama", "Alaska", "Arizona", "Arkansas", "California",
-                "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida",
-                "Georgia", "Guam", "Hawaii", "Idaho", "Illinois",
-                "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
-                "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
-                "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada",
-                "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina",
-                "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
-                "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee",
-                "Texas", "Utah", "Vermont", "Virginia", "Virgin Islands",
-                "Washington", "West Virginia", "Wisconsin", "Wyoming"
-              ]);
-              break;
-            case 'country':
-              callback([
-                "China", "India", "United States", "Indonesia", "Brazil",
-                "Pakistan", "Bangladesh", "Nigeria", "Russia", "Japan",
-                "Mexico", "Philippines", "Vietnam", "Ethiopia", "Egypt",
-                "Germany", "Turkey", "Iran", "Thailand", "D. R. of Congo",
-                "France", "United Kingdom", "Italy", "Myanmar", "South Africa",
-                "South Korea", "Colombia", "Ukraine", "Spain", "Tanzania",
-                "Sudan", "Kenya", "Argentina", "Poland", "Algeria",
-                "Canada", "Uganda", "Morocco", "Iraq", "Nepal",
-                "Peru", "Afghanistan", "Venezuela", "Malaysia", "Uzbekistan",
-                "Saudi Arabia", "Ghana", "Yemen", "North Korea", "Mozambique",
-                "Taiwan", "Syria", "Ivory Coast", "Australia", "Romania",
-                "Sri Lanka", "Madagascar", "Cameroon", "Angola", "Chile",
-                "Netherlands", "Burkina Faso", "Niger", "Kazakhstan", "Malawi",
-                "Cambodia", "Guatemala", "Ecuador", "Mali", "Zambia",
-                "Senegal", "Zimbabwe", "Chad", "Cuba", "Greece",
-                "Portugal", "Belgium", "Czech Republic", "Tunisia", "Guinea",
-                "Rwanda", "Dominican Republic", "Haiti", "Bolivia", "Hungary",
-                "Belarus", "Somalia", "Sweden", "Benin", "Azerbaijan",
-                "Burundi", "Austria", "Honduras", "Switzerland", "Bulgaria",
-                "Serbia", "Israel", "Tajikistan", "Hong Kong", "Papua New Guinea",
-                "Togo", "Libya", "Jordan", "Paraguay", "Laos",
-                "El Salvador", "Sierra Leone", "Nicaragua", "Kyrgyzstan", "Denmark",
-                "Slovakia", "Finland", "Eritrea", "Turkmenistan"
-              ], {preserveOrder: true});
-              break;
-          }
+		  $.ajax("/values/" + facet + "/" + searchTerm, {
+		         type:"GET",
+		         dataType:"json",
+		         success:function (res) {
+		            callback(res);
+		        }
+		    });	
         }
       }
     });
